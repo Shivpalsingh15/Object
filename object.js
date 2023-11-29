2ca294c89a8ae6e9437204042d6ed92d674b6ac4
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2023-11-29 23:34:51 +0530

    Objects basics

---
+++ object.js
@@ -0,0 +1,12 @@
+//Objects
+
+let person = {
+    name: "John",
+    age: 30,
+    occupation: "Engineer",
+};
+
+// console.log('person:', person.age)//person: 30
+// console.log('person:', person.name)//person: john
+// console.log('person:', person["occupation"])//person: Engineer
+
